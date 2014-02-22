fcac766c22259fa41905d929566efef6251d9906
brianschmitz <brian_schmitz@mac.com>
2014-02-22 10:10:33 -0800

    Merge branch 'master' of https://github.com/UWPCE-PythonCert/training.python_web
    Conflicts:
    	resources/session04/wsgi/bookapp.py

--- assignments/session07/mysite/myblog/admin.py
+++ assignments/session07/mysite/myblog/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from myblog.models import Post
 from myblog.models import Category
+from myblog.models import PostAdmin
+from myblog.models import CategoryAdmin
+from myblog.models import CategoryInline
 
-admin.site.register(Post)
-admin.site.register(Category)
+admin.site.register(Post, PostAdmin, )
+admin.site.register(Category, CategoryAdmin)
+# admin.site.register(CategoryInline, )
+
--- assignments/session07/mysite/myblog/models.py
+++ assignments/session07/mysite/myblog/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib import admin
+
 
 class Post(models.Model):
-	title = models.CharField(max_length=128)
-	text = models.TextField(blank=True)
-	author = models.ForeignKey(User)
-	created_date = models.DateTimeField(auto_now_add=True)
-	modified_date = models.DateTimeField(auto_now=True)
-	published_date = models.DateTimeField(blank=True, null=True)
+    title = models.CharField(max_length=128)
+    text = models.TextField(blank=True)
+    author = models.ForeignKey(User)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(blank=True, null=True)
 
-	def __unicode__(self):
-		return self.title
+    def __unicode__(self):
+        return self.title
+
 
 class Category(models.Model):
     name = models.CharField(max_length=128)
@@ -20,3 +23,28 @@ class Category(models.Model):
 
     def __unicode__(self):
         return self.name
+
+
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'description')
+    exclude = ('posts', )
+
+    def __unicode__(self):
+        return self.name
+
+
+class CategoryInline(admin.TabularInline):
+    model = Category
+
+
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'author', 'created_date', )
+    inlines = [CategoryInline, ]
+
+    def __unicode__(self):
+        return self.name
+
+
+
